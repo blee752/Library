4861d90131c17ce79f1833bf9a7c5cb7e35cec0c
Brian <Blee752@mtroyal.ca>
2021-12-16 01:13:12 -0700

    basic layout created along with some premade library entries to test apperances

--- Scripts.js
+++ Scripts.js
@@ -17,6 +17,28 @@ function addBookToLibrary(){
     //do something
 }
 
-const test = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-test.info();
+const demo = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 
+const demo2 = new book('The Fellowship Of The Ring', 'J.R.R. Tolkien', 448, 'not read yet');
+demo.info();
+
+myLibrary.push(demo);
+myLibrary.push(demo2);
+
+let cell = document.createElement('div');
+let title = document.createElement('h3');
+let author = document.createElement('p');
+let pages = document.createElement('p');
+let read = document.createElement('p');
+const libraryContainer = document.querySelector('.main-container');
+title.innerText = myLibrary[1].title;
+author.innerText = myLibrary[1].author;
+pages.innerText = myLibrary[1].pages;
+read.innerText = myLibrary[1].read;
+
+cell.appendChild(title);
+cell.appendChild(author);
+cell.appendChild(pages);
+cell.appendChild(read);
+libraryContainer.appendChild(cell);
+//container.appendChild(cell);
