8ca522b57e18a2dd157e596d903d663dc1c1c63a
Brian <Blee752@mtroyal.ca>
2021-12-16 23:30:53 -0700

    Created function to loop through library array and add them into the library collection

--- Scripts.js
+++ Scripts.js
@@ -17,6 +17,7 @@ function addBookToLibrary(){
     //do something
 }
 
+//test entries
 const demo = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 
 const demo2 = new book('The Fellowship Of The Ring', 'J.R.R. Tolkien', 448, 'not read yet');
@@ -25,20 +26,46 @@ demo.info();
 myLibrary.push(demo);
 myLibrary.push(demo2);
 
-let cell = document.createElement('div');
-let title = document.createElement('h3');
-let author = document.createElement('p');
-let pages = document.createElement('p');
-let read = document.createElement('p');
-const libraryContainer = document.querySelector('.main-container');
-title.innerText = myLibrary[1].title;
-author.innerText = myLibrary[1].author;
-pages.innerText = myLibrary[1].pages;
-read.innerText = myLibrary[1].read;
+let cell, title, author, pages, read;
 
+const libraryContainer = document.querySelector('.grid-container');
+
+/* //setting book information into the corresponding elements for the container
+title.innerText = `${myLibrary[1].title}`;
+author.innerText = `By ${myLibrary[1].author}`;
+pages.innerText = `Pages: ${myLibrary[1].pages}`;
+read.innerText = `Status: ${myLibrary[1].read}`;
+
+//append book info to the container div
 cell.appendChild(title);
 cell.appendChild(author);
 cell.appendChild(pages);
 cell.appendChild(read);
-libraryContainer.appendChild(cell);
-//container.appendChild(cell);
+//append the library entry into the library div
+libraryContainer.appendChild(cell); */
+
+//Function to loop through mylibrary and create the elements to append
+function createLibrary() {
+    myLibrary.forEach((book) => {
+        cell = document.createElement('div');
+        title = document.createElement('h3');
+        author = document.createElement('p');
+        pages = document.createElement('p');
+        read = document.createElement('p');
+        
+        title.innerText = `${book.title}`;
+        author.innerText = `By ${book.author}`;
+        pages.innerText = `Pages: ${book.pages}`;
+        read.innerText = `Status: ${book.read}`;
+        
+        cell.appendChild(title);
+        cell.appendChild(author);
+        cell.appendChild(pages);
+        cell.appendChild(read);
+        libraryContainer.appendChild(cell);
+    })
+}
+createLibrary();
+
+//On load, check if mylibrary isnt empty, if not run loop function to add entries onto webpage
+
